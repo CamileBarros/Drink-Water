9ede0d836642a165875bc32c963d0c43deb813ec
camilebarros <camilebarrost@gmail.com>
2021-11-25 13:25:09 -0300

    Beba agua almost done

--- app/src/main/java/com/dio/drinkwater/MainActivity.kt
+++ app/src/main/java/com/dio/drinkwater/MainActivity.kt
@@ -26,11 +26,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Thread.sleep(1000)
-
-        setTheme(R.style.Theme_DrinkWater)
-
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
